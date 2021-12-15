87816a8cbf4c1e13eafad9a2dc650904f784d66e
nay <95525703+Na-y@users.noreply.github.com>
2021-12-15 09:57:09 +0630

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+let site_detail = {
+    'site_name': {
+        'tc': 'Fantasy-novels',
+        'url': 'https://na-y.github.io'
+    },
+    'site_home': {
+        'tc': 'Home',
+        'url': 'https://na-y.github.io/fantasy-novels'
+    },
+    'products': {
+        'tc': 'Our Products',
+        'url': 'https://na-y.github.io/fantasy-novels/products/index.html'
+    }
+}
+
+export {site_detail}
